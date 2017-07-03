e3a0beafe44b7d4820667b7412c7747fc67f704d
Andreas Lefdalsnes <andreas.lefdalsnes@outlook.com>
2017-07-03 03:03:16 +0200

    Finish grid, just needs styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
     var rows = 16;
-    var columns = 16;
+    var columns = rows;
+    var width = "620px"
+    var height = width;
+    createGrid(rows, columns, width, height);
+});
+
+function createGrid(rows, columns, width, height) {
     var $container = $('<div>', {id: "container"});
     var $row = $('<div>', {class: "row"});
     var $square = $('<div>', {class: "square"});
+    $container.css("width", width);
+    $container.css("height", height);
+    $square.css("width", 100/rows + "%");
+    $square.css("height", 100/columns + "%");
     $("body").append($container);
 
     for(var i=0; i<columns; i++) {
@@ -12,4 +22,20 @@ $(document).ready(function() {
     for(var j=0; j<rows; j++) {
         $container.append($row.clone());
     }
-});
+
+    $(".square").hover(
+        function() {$(this).css("background-color", "yellow");},
+        function() {$(this).css("background-color", "pink");}
+    );
+
+    $("#clear").click(function() {
+        $(".square").css("background-color", "#FFFFFF");
+    });
+
+    $("#resize").click(function() {
+        rows = prompt("Please enter grid size", "16");
+        columns = rows;
+        $("#container").empty();
+        createGrid(rows, columns, width, height);
+    });
+}
